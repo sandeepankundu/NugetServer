63759902ccebe38f73092c2821aed90676dce20d
Sandeepan Kundu <sandeepan@hellogbye.com>
2015-04-09 17:25:41 -0400

    Nuget Server + DummyLibrary (for testing the initial hosting)

---
+++ DummyLibrary/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DummyLibrary
+{
+    public class DummyClass
+    {
+        public int Property { get; set; }
+
+        public string Greet(string name)
+        {
+            return string.Format("{0} {1}!", "Hello", name);
+        }
+
+        public string GreetBye(string name)
+        {
+            return string.Format("{0} {1}!", "Bye", name);
+        }
+    }
+}
